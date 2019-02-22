5ff7695bb9822def240c30e9f45dbe75c568a828
KKraan <kraankees@gmail.com>
2019-02-22 14:48:58 +0100

    Ja

--- accesswordxml.py
+++ accesswordxml.py
@@ -1,28 +1,34 @@
 from xml.etree.cElementTree import XML
 import zipfile
+import os
+import pickle
 
 WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 PARA = WORD_NAMESPACE + 'p'
 TEXT = WORD_NAMESPACE + 't'
+expertise = []
+competencies = []
 
-document = zipfile.ZipFile('Kraan, Kees.docx')
-xml_content = document.read('word/document.xml')
-document.close()
-tree = XML(xml_content)
+for file in os.listdir('./CV1/'):
+    if file.endswith(".docx"):
+        document = zipfile.ZipFile('./CV1/'+file)
+        xml_content = document.read('word/document.xml')
+        document.close()
+        tree = XML(xml_content)
 
-array = []
-for paragraph in tree.getiterator(PARA):
-    for node in paragraph.getiterator(TEXT):
-        if node.text:
-            # print(node.text)
-            array.append(node.text)
-            break
+        array = []
+        for paragraph in tree.getiterator(PARA):
+            for node in paragraph.getiterator(TEXT):
+                if node.text:
+                    array.append(node.text)
+        expertise.append(array[array.index('Expertise')+1: array.index('Kerncompetenties')])
+        competencies.append(array[array.index('Kerncompetenties')+1: array.index('Profiel')])
+    print(file)
 
-print(array[10])
-expertise= []
-competencies= []
-expertise.append(array[array.index('Expertise')+1:array.index('Kerncompetenties')])
-competencies.append(array[array.index('Kerncompetenties')+1:array.index('Profiel')])
+with open('expertise.pickle', 'wb') as f:
+    pickle.dump(expertise, f)
+with open('competencies.pickle', 'wb') as f:
+    pickle.dump(expertise, f)
 
-print('expertise = ' + str(expertise))
-print('competencies = ' + str(competencies))
+# print('expertise = ' + str(expertise))
+# print('competencies = ' + str(competencies))
---
+++ test.py
@@ -0,0 +1,4 @@
+import pickle
+
+with open('expertise.pickle', 'rb') as handle:
+    b = pickle.load(handle)
