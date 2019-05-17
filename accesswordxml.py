a1e5368a037f7c3ad9cad3654f470eb059afe9ec
Kees <kees.kraan@vng.nl>
2019-05-17 10:55:25 +0200

    script opschonen

--- accesswordxml.py
+++ accesswordxml.py
@@ -1,22 +1,38 @@
+# IMPORT LIBRARIES
 from xml.etree.cElementTree import XML
 import zipfile
 import os
 import pickle
 import csv
 
+# Declare specific variables for MS WORD
 WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 PARA = WORD_NAMESPACE + 'p'
 TEXT = WORD_NAMESPACE + 't'
+
+
+def write_csv(filename,data):
+    with open(filename, 'w') as f:
+        wr = csv.writer(f)
+        wr.writerow(data)
+
+def write_pickle(filename,data):
+    with open(filename, 'w') as f:
+        pickle.dump(data, f)
+
+def read_worddoc(filename):
+    document = zipfile.ZipFile('./CV1/' + file)
+    xml_content = document.read('word/document.xml')
+    document.close()
+    tree = XML(xml_content)
+    return tree
+
+
 expertise = []
 competencies = []
-
 for file in os.listdir('./CV1/'):
     if file.endswith(".docx"):
-        document = zipfile.ZipFile('./CV1/'+file)
-        xml_content = document.read('word/document.xml')
-        document.close()
-        tree = XML(xml_content)
-
+        tree=read_worddoc('./CV1/'+file)
         array = []
         for paragraph in tree.getiterator(PARA):
             for node in paragraph.getiterator(TEXT):
@@ -30,17 +46,10 @@ for file in os.listdir('./CV1/'):
         competencies.append(array[array.index('Kerncompetenties')+1: end])
     print(file)
 
-with open('result/expertise.pickle', 'wb') as f:
-    pickle.dump(expertise, f)
-with open('result/competencies.pickle', 'wb') as f:
-    pickle.dump(competencies, f)
 
-with open('result/expertise.csv', 'w') as f:
-    wr = csv.writer(f)
-    wr.writerow(expertise)
-with open('result/competencies.csv', 'w') as f:
-    wr = csv.writer(f)
-    wr.writerow(competencies)
+write_pickle('result/expertise.pickle',expertise)
+write_pickle('result/competencies.pickle',competencies)
 
-# print('expertise = ' + str(expertise))
-# print('competencies = ' + str(competencies))
+write_csv('result/expertise.csv',expertise)
+write_csv('result/competencies.csv',competencies)
+
